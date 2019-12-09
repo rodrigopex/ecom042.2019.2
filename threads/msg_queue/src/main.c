06e3d64c92539ae5486c1476a39134b5326ab1ba
Rodrigo Peixoto <rodrigopex@ic.ufal.br>
2019-12-09 12:50:37 -0300

    Finaliza semáforo e adiciona message queue
    Signed-off-by: Rodrigo Peixoto <rodrigopex@ic.ufal.br>

--- threads/msg_queue/src/main.c
+++ threads/msg_queue/src/main.c
@@ -15,15 +15,30 @@
 /* scheduling priority used by each thread */
 #define PRIORITY 7
 
+K_MSGQ_DEFINE(pipe, 4, 10, 4);
+
 void main()
 {
-    printk("Hello world!\n");
+    printk("Main!\n");
+    u32_t counter = 0;
     while (1) {
         k_sleep(K_MSEC(1000));
-        printk(".\n");
+        printk(". main\n");
+        counter++;
+        while (!k_msgq_put(&pipe, &counter, K_SECONDS(1))) {
+        }
+    }
+}
+void t2()
+{
+    printk("t2!\n");
+    u32_t data = 0;
+    while (1) {
+        k_msgq_get(&pipe, &data, K_FOREVER);
+        printk(". %d\n", data);
     }
 }
 
-// K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL, PRIORITY, 0,
-// K_NO_WAIT); K_THREAD_DEFINE(blink2_id, STACKSIZE, blink2, NULL, NULL, NULL, PRIORITY,
-// 0, K_NO_WAIT);
+K_THREAD_DEFINE(t2_id, STACKSIZE, t2, NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
+// K_THREAD_DEFINE(blink2_id, STACKSIZE, blink2, NULL, NULL, NULL, PRIORITY, 0,
+// K_NO_WAIT);
--- threads/sema/src/main.c
+++ threads/sema/src/main.c
@@ -15,39 +15,55 @@
 /* scheduling priority used by each thread */
 #define PRIORITY 0
 
+
+K_SEM_DEFINE(t3_sema, 0, 1);
+K_SEM_DEFINE(var_sema, 1, 1);
+u8_t var = 0;
+
 void main()
 {
     printk("Hello main thread!\n");
     while (1) {
         k_sleep(K_SECONDS(1));
-        printk(". main\n");
+        printk(". main %d\n", var);
     }
 }
 
-u8_t counter = 0;
 void thread2()
 {
+    u8_t counter = 0;
     printk("Hello thread 2!\n");
     while (1) {
         k_sleep(K_MSEC(300));
         printk(". thread 2: %d\n", counter);
         counter++;
+        k_sem_take(&var_sema, K_FOREVER);
+        var = 2;
+        if (var == 2) {
+            var = 0;
+        }
+        k_sem_give(&var_sema);
         if (counter >= 10) {
             counter = 0;
-        }
-    }
-}
-void thread3()
-{
-    printk("Hello thread 3!\n");
-    while (1) {
-        if (counter == 9) {
-            printk(". thread 3\n");
-        } else {
-            k_sleep(1);
+            k_sem_give(&t3_sema);
         }
     }
 }
 
-K_THREAD_DEFINE(thread2_id, STACKSIZE, thread2, NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
+void thread3()
+{
+    printk("Hello thread 3!\n");
+    while (1) {
+        if (k_sem_take(&t3_sema, K_MSEC(100))) {
+            k_sem_take(&var_sema, K_FOREVER);
+            var = 3;
+            k_sem_give(&var_sema);
+        } else {
+            printk(". --------- thread 3\n");
+        }
+    }
+}
+
+K_THREAD_DEFINE(thread2_id, STACKSIZE, thread2, NULL, NULL, NULL, PRIORITY + 1, 0,
+                K_NO_WAIT);
 K_THREAD_DEFINE(thread3_id, STACKSIZE, thread3, NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
