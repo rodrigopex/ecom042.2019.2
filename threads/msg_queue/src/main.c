06705730e019d7a996857976f50248d1efe80cf4
Rodrigo Peixoto <rodrigopex@ic.ufal.br>
2019-12-11 12:51:16 -0300

    Finaliza a aula de thread com message queue.
    Signed-off-by: Rodrigo Peixoto <rodrigopex@ic.ufal.br>

--- threads/msg_queue/src/main.c
+++ threads/msg_queue/src/main.c
@@ -15,18 +15,25 @@
 /* scheduling priority used by each thread */
 #define PRIORITY 7
 
-K_MSGQ_DEFINE(pipe, 4, 10, 4);
+K_MSGQ_DEFINE(pipe, sizeof(u32_t), 10, 4);
+
+struct data_t {
+    u16_t lo;
+    u16_t hi;
+    u16_t average;
+};
+
+K_MSGQ_DEFINE(data_pipe, sizeof(struct data_t), 2, 2);
 
 void main()
 {
     printk("Main!\n");
+    printk("Size of struct data = %d\n", sizeof(struct data_t));
     u32_t counter = 0;
-    while (1) {
-        k_sleep(K_MSEC(1000));
-        printk(". main\n");
+    while (!k_msgq_put(&pipe, &counter, K_FOREVER)) {
+        printk(". main put %d\n", counter);
         counter++;
-        while (!k_msgq_put(&pipe, &counter, K_SECONDS(1))) {
-        }
+        k_busy_wait(500000);
     }
 }
 void t2()
@@ -35,10 +42,38 @@ void t2()
     u32_t data = 0;
     while (1) {
         k_msgq_get(&pipe, &data, K_FOREVER);
-        printk(". %d\n", data);
+        struct data_t new_data = {
+            .lo = data, .hi = data >> 16, .average = (new_data.hi + new_data.lo) >> 1};
+        k_msgq_put(&data_pipe, &new_data, K_FOREVER);
+        printk(". t2 -> %d\n", data);
     }
 }
 
+void t3()
+{
+    printk("t3!\n");
+    u32_t data = 0;
+    while (1) {
+        struct data_t new_data = {0};
+        k_msgq_get(&data_pipe, &new_data, K_FOREVER);
+        printk(". t3 -> hi = %d, lo = %d, av = %d\n", new_data.hi, new_data.lo,
+               new_data.average);
+    }
+}
+
+void t4()
+{
+    printk("t4!\n");
+    u32_t data = 0;
+    while (1) {
+        struct data_t new_data = {0};
+        k_msgq_get(&data_pipe, &new_data, K_FOREVER);
+        printk(". t4 -> hi = %d, lo = %d, av = %d\n", new_data.hi, new_data.lo,
+               new_data.average);
+    }
+}
+
+
 K_THREAD_DEFINE(t2_id, STACKSIZE, t2, NULL, NULL, NULL, PRIORITY, 0, K_NO_WAIT);
-// K_THREAD_DEFINE(blink2_id, STACKSIZE, blink2, NULL, NULL, NULL, PRIORITY, 0,
-// K_NO_WAIT);
+K_THREAD_DEFINE(t3_id, STACKSIZE, t3, NULL, NULL, NULL, PRIORITY - 1, 0, K_NO_WAIT);
+K_THREAD_DEFINE(t4_id, STACKSIZE, t4, NULL, NULL, NULL, PRIORITY - 2, 0, K_NO_WAIT);
